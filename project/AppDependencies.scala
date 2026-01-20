372084c6790afe0e245ec63884ff05ada6a3b6b6
jonathanrea <65158821+jonathanrea@users.noreply.github.com>
2026-01-20 11:11:04 +0000

    Upgrading dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,16 @@ import play.sbt.PlayImport.caffeine
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "10.4.0"
-  val playPartialsVersion = "9.1.0"
-  val playFrontendHMRCVersion = "11.13.0"
-  val catsVersion = "2.12.0"
-  val scalaTestPlusVersion = "7.0.1"
-  val jsoupVersion = "1.18.1"
-  val mockitoVersion = "5.11.0"
-  val scalaMockVersion = "5.2.0"
+  val bootstrapPlayVersion = "10.5.0"
+  val playPartialsVersion = "10.2.0"
+  val playFrontendHMRCVersion = "12.22.0"
+  val catsVersion = "2.13.0"
+  val scalaTestPlusVersion = "7.0.2"
+  val jsoupVersion = "1.22.1"
+  val mockitoVersion = "5.21.0"
+  val scalaMockVersion = "7.5.3"
   val wiremockVersion = "3.0.0-beta-7"
-  val hmrcMongoVersion = "2.6.0"
+  val hmrcMongoVersion = "2.11.0"
   val playVersion = "play-30"
 
   val compile = Seq(
@@ -24,7 +24,7 @@ object AppDependencies {
     "org.typelevel" %% "cats-core" % catsVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion" % hmrcMongoVersion,
     "uk.gov.hmrc" %% s"play-frontend-hmrc-$playVersion" % playFrontendHMRCVersion,
-    "uk.gov.hmrc" %% s"crypto-json-$playVersion" % "8.2.0",
+    "uk.gov.hmrc" %% s"crypto-json-$playVersion" % "8.4.0",
     "org.jsoup" % "jsoup" % jsoupVersion
   )
 
@@ -34,11 +34,11 @@ object AppDependencies {
     "org.jsoup" % "jsoup" % jsoupVersion % Test,
     "org.mockito" % "mockito-core" % mockitoVersion % Test,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion % Test,
-    "org.scalacheck" %% "scalacheck" % "1.18.1" % Test,
+    "org.scalacheck" %% "scalacheck" % "1.19.0" % Test,
     "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test,
     "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapPlayVersion % Test,
     caffeine,
-    "uk.gov.hmrc" %% s"crypto-json-$playVersion" % "8.1.0" % Test,
+    "uk.gov.hmrc" %% s"crypto-json-$playVersion" % "8.4.0" % Test,
   )
 
   val it = Seq.empty
